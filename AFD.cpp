531d2626bddb71e982884d0c565acba8a0368be1
Elink <eliassegundo.segundo@gmail.com>
2017-03-08 08:00:47 -0600

    Version preliminar del programa, realizado en 1 hora

--- AFD.cpp
+++ AFD.cpp
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class node{
-
-public:
-	int stateNumber;
+struct node
+{
 	map<char,int> transitions;
 	bool endState;
 	bool startState;
@@ -18,35 +15,71 @@ protected:
 private:
 	vector<node> graph;
 	vector<char> alphabet;
-public:
-	int evaluateLetter(char letter)
-	{
-		//Revisara la letra y regresara un estado para seguir evaluando
-
-		return 0;
-	}
+	int position;
 
 	bool isInAlphabet(char letter)
 	{
-		//Verificara que la palabra este en el alfabeto
+		for(int  i=0;i<alphabet.size();i++)
+		{
+			if(letter==alphabet[i])return true;
+		}
 		return false;
 	}
+public:
+	Automaton();
+	~Automaton();
+
+	bool isAcceptedWord(string word)
+	{
+		for(int i=0;i<graph.size();i++)
+		{
+			if(graph[i].startState)
+			{
+				position=i;
+				break;
+			}
+		}
+
+		for(int i=0;i<word.size();i++)
+		{
+			if(isInAlphabet(word[i]))
+			{
+
+				position=graph[position].transitions[word[i]];
+			}
+			else return false;	
+		}
+		
+		if(graph[position].endState)return true;
+		return false;
+	}
+
+	
 
 	void setGraph(vector<node> g)
 	{
 		//metodo que copiara el grafo de entrada a esta clase Automata
+		for(int i=0;i<g.size();i++)
+		{
+			this->graph.push_back(g[i]);
+		}
 	}
 
-	void setAlphabet(vector<char> a)
+	void setAlphabet(string a)
 	{
-		//metodo que copiara el alfabeto de entrada
+		for(int i=0;i<a.size();i++)
+		{
+			this->alphabet.push_back(a[i]);
+		}
 	}
 
 
 
 };
 
+
 int main()
 {
+
 
 }
